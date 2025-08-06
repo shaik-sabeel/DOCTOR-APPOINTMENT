39072641dea94e7c1357229bdc9823febbc6221c
shaik-sabeel <techsabeel1919@gmail.com>
2025-08-07 00:35:53 +0530

    Appointment

--- front-end/src/pages/Appointment.jsx
+++ front-end/src/pages/Appointment.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useParams } from 'react-router-dom'
 
 const Appointment = () => {
+
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+
+const [docInfo, setDocInfo] = useState(null)
+
+  const fetchDocInfo = async()=>{
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo)
+    console.log(docInfo)
+  }
+
+  useEffect(() =>{
+    fetchDocInfo()
+  }, [doctors,docId])
+
   return (
     <div>
       
